b4cd18f56a59bed82071ed83ede9106cd4759dbe
glen654 <glenalloy@gmail.com>
2024-11-09 19:03:07 +0530

    implemented health check controller

---
+++ src/main/java/lk/ijse/green_shadow/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package lk.ijse.green_shadow.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String healthTest(){
+        return "Green Shadow API is working";
+    }
+}
