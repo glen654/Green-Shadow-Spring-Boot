4332ffd0cf5fea46a9346e823d60bab34c839a02
glen654 <glenalloy@gmail.com>
2024-11-10 12:32:57 +0530

    equipment controller implemented

---
+++ src/main/java/lk/ijse/green_shadow/controller/EquipmentController.java
@@ -0,0 +1,80 @@
+package lk.ijse.green_shadow.controller;
+
+import lk.ijse.green_shadow.customStatusCodes.SelectedErrorStatus;
+import lk.ijse.green_shadow.dto.EquipmentStatus;
+import lk.ijse.green_shadow.dto.impl.EquipmentDTO;
+import lk.ijse.green_shadow.exception.DataPersistException;
+import lk.ijse.green_shadow.exception.EquipmentNotFoundException;
+import lk.ijse.green_shadow.service.EquipmentService;
+import lk.ijse.green_shadow.util.Regex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/equipment")
+public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO) {
+        try {
+            equipmentService.saveEquipment(equipmentDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping(value = "/{equipmentId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public EquipmentStatus getSelectedEquipment(@PathVariable ("equipmentId") String equipmentId) {
+        if (!Regex.equipIdMatcher(equipmentId)) {
+            return new SelectedErrorStatus(1, "Equipment id does not match");
+        }
+        return equipmentService.getEquipment(equipmentId);
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EquipmentDTO> getAllEquipments() {
+        return equipmentService.getAllEquipment();
+    }
+    @DeleteMapping(value = "/{equipmentId}")
+    public ResponseEntity<Void> deleteEquipment(@PathVariable("equipmentId") String equipmentId) {
+        try {
+            if(!Regex.equipIdMatcher(equipmentId)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            equipmentService.deleteEquipment(equipmentId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (EquipmentNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PutMapping(value = "/{equipmentId}")
+    public ResponseEntity<Void> updateEquipment(@PathVariable ("equipmentId") String equipmentId,
+                                                @RequestBody EquipmentDTO equipmentDTO) {
+
+        try {
+            if(!Regex.equipIdMatcher(equipmentId) || equipmentDTO == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            equipmentService.updateEquipment(equipmentId, equipmentDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (EquipmentNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
