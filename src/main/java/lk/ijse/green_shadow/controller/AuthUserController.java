41b4a79a0073f4e5b23fe7385c2bfc123ced4748
glen654 <glenalloy@gmail.com>
2024-11-11 21:29:35 +0530

    implemented authusercontroller

---
+++ src/main/java/lk/ijse/green_shadow/controller/AuthUserController.java
@@ -0,0 +1,46 @@
+package lk.ijse.green_shadow.controller;
+
+import lk.ijse.green_shadow.dto.impl.UserDTO;
+import lk.ijse.green_shadow.exception.DataPersistException;
+import lk.ijse.green_shadow.secure.JWTAuthResponse;
+import lk.ijse.green_shadow.secure.SignIn;
+import lk.ijse.green_shadow.service.AuthService;
+import lk.ijse.green_shadow.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth/")
+@RequiredArgsConstructor
+public class AuthUserController {
+    private final UserService userService;
+    private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping(value = "signup",consumes = MediaType.APPLICATION_JSON_VALUE,
+                                    produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JWTAuthResponse> saveUser(@RequestBody UserDTO userDTO) {
+        try {
+            userService.saveUser(userDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping(value = "signin",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn) {
+        return ResponseEntity.ok(authService.signIn(signIn));
+    }
+    @PostMapping("refresh")
+    public ResponseEntity<JWTAuthResponse> refreshToken(@RequestParam ("existingToken") String existingToken) {
+        return ResponseEntity.ok(authService.refreshToken(existingToken));
+    }
+}
