0c799ad2c7f5ade88bcf05c9a15a536d720ebac4
glen654 <glenalloy@gmail.com>
2024-11-14 22:10:14 +0530

    implemented staff controller class

---
+++ src/main/java/lk/ijse/green_shadow/controller/StaffController.java
@@ -0,0 +1,83 @@
+package lk.ijse.green_shadow.controller;
+
+import lk.ijse.green_shadow.customStatusCodes.SelectedErrorStatus;
+import lk.ijse.green_shadow.dto.StaffStatus;
+import lk.ijse.green_shadow.dto.impl.StaffDTO;
+import lk.ijse.green_shadow.exception.DataPersistException;
+import lk.ijse.green_shadow.exception.StaffNotFoundException;
+import lk.ijse.green_shadow.service.StaffService;
+import lk.ijse.green_shadow.util.Regex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@RestController
+@RequestMapping("api/v1/staff")
+public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO) {
+        try {
+            staffService.saveStaff(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping(value="/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public StaffStatus getSelectedStaff(@PathVariable ("id") String id){
+        if(!Regex.staffIdMatcher(id)){
+            return new SelectedErrorStatus(1,"Staff ID does not match");
+        }
+        return staffService.getStaff(id);
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<StaffDTO> getAllStaff(){
+        return staffService.getAllStaff();
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteStaff(@PathVariable("id") String id){
+        try {
+            if(!Regex.staffIdMatcher(id)){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.deleteStaff(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (StaffNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> updateStaff(@PathVariable ("id") String id,
+                                            @RequestBody StaffDTO staffDTO){
+
+        try {
+            if(!Regex.staffIdMatcher(id) || staffDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.updateStaff(id, staffDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (StaffNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
